cb52bca53623c486663d244fa2d13a1cb12ebdd3
Kostyantyn Chernyk <chernykko@gmail.com>
2023-07-26 12:36:46 +0100

    Installed and registered crizpy_forms, created CreateTaskView with url and template

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from core.views import index
+from core.views import index, TaskCreateView
 
 app_name = "core"
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("task_create", TaskCreateView.as_view(), name="create-task")
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
 from core.models import Task
 
@@ -10,3 +12,9 @@ def index(request):
         "tasks": tasks
     }
     return render(request, "core/index.html", context)
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("core:index")
